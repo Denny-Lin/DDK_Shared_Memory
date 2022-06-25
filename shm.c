cf296342b7cf0eee787f1bc79f21276498ce2c02
DDK <67073582+Denny-Lin@users.noreply.github.com>
2022-06-26 05:37:57 +0800

    Added some safeguards in example().

--- shm.c
+++ shm.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
-int PAGESIZE = 100;
+static int SUCCESS = 0;
+static int ERROR = -1;
 
 int print(char* argv){
     printf("hello %s\n", argv);
@@ -14,16 +16,31 @@ int print(char* argv){
 
 int example(){
     int value = 5;
-    int* shared_memory = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    int PAGESIZE = 100;
+    int* shared_memory;
+    pid_t pid = 0;
+    
+    shared_memory = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    if(shared_memory == MAP_FAILED){      
+        return ERROR;
+    }
     
     *shared_memory = 20;
     
-    if(fork()==0){
+    pid = fork();
+    
+    if(pid == 0){
         value = 10;       
         *shared_memory = 200;    
            
         printf("child: value = %d\n", value);
         printf("child: *shared_memory = %i\n", *shared_memory); 
+        
+        exit(SUCCESS);
+    }
+    else if(pid == -1){
+        return ERROR;
     }
     else{
         wait(NULL);
@@ -32,5 +49,5 @@ int example(){
         printf("parent: *shared_memory = %i\n", *shared_memory); 
     }   
     
-    return 0;
+    return SUCCESS;
 }
--- shm.h
+++ shm.h
@@ -1,8 +1,10 @@
 #ifndef __SHM_H__
 #define __SHM_H__
 
+//test
 int print(char* argv);
 
+//mmap()
 int example();
 
 #endif /*__SHM_H__*/
